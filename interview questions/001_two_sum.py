2c398e519466ddaa35aa6984d839b2a960c30129
Hadi Samadzad <hadisamadzad@Hadis-MBP.broadband>
2023-06-12 21:54:36 +0100

    Add question number 1

---
+++ interview questions/001_two_sum.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/two-sum/?envType=featured-list&envId=top-interview-questions
+def twoSum(nums, target):
+
+    n = len(nums)
+    map_dict = {}
+
+    for index, value in enumerate(nums):
+
+        if not (value in map_dict):
+            map_dict[value] = index
+
+        for index in reversed(range(n)):
+            diff = target - nums[index]
+            if diff in map_dict and index != map_dict[diff]:
+                return [map_dict[diff], index]
+
+
+print(twoSum(nums=[2, 7, 11, 15], target=9))
--- interview questions/003_longest_substring_without_repeating_characters.py
+++ interview questions/003_longest_substring_without_repeating_characters.py
@@ -1,28 +1,28 @@
-#https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=featured-list&envId=top-interview-questions
-s = "dvdf"
-expected = 3
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=featured-list&envId=top-interview-questions
+def lengthOfLongestSubstring(s):
+    n = len(s)
 
-n = len(s)
-maxlen = 0
-word_map = {}
-start = 0
+    maxlen = 0
+    word_map = {}
+    start = 0
 
-for i in range(n):
-    char = s[i]
-    is_char_in_map = char in word_map
-    char_in_map_index = word_map[char] if is_char_in_map else None
+    for i in range(n):
+        char = s[i]
 
-    if not is_char_in_map:
-        word_map[char] = i
-        maxlen = max(maxlen, i - start + 1)
+        if not char in word_map:
+            word_map[char] = i
+            maxlen = max(maxlen, i - start + 1)
 
-    elif char_in_map_index >= start:
-        maxlen = max(maxlen, i - start)
-        start = word_map[char] + 1
-        word_map[char] = i
+        elif word_map[char] >= start:
+            maxlen = max(maxlen, i - start)
+            start = word_map[char] + 1
+            word_map[char] = i
 
-    else:
-        maxlen = max(maxlen, i - start + 1)
-        word_map[char] = i
+        else:
+            maxlen = max(maxlen, i - start + 1)
+            word_map[char] = i
 
-print(expected, maxlen)
+    return maxlen
+
+
+print(lengthOfLongestSubstring("pwwkew"))
