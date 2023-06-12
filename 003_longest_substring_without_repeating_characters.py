4e08c4695a365da11ebd419093d84fb3b13f4cf6
Hadi Samadzad <hadisamadzad@Hadis-MBP.broadband>
2023-06-12 21:39:11 +0100

    Add interview question number 3

---
+++ 003_longest_substring_without_repeating_characters.py
@@ -0,0 +1,27 @@
+s = "dvdf"
+expected = 3
+
+n = len(s)
+maxlen = 0
+word_map = {}
+start = 0
+
+for i in range(n):
+    char = s[i]
+    is_char_in_map = char in word_map
+    char_in_map_index = word_map[char] if is_char_in_map else None
+
+    if not is_char_in_map:
+        word_map[char] = i
+        maxlen = max(maxlen, i - start + 1)
+
+    elif char_in_map_index >= start:
+        maxlen = max(maxlen, i - start)
+        start = word_map[char] + 1
+        word_map[char] = i
+
+    else:
+        maxlen = max(maxlen, i - start + 1)
+        word_map[char] = i
+
+print(expected, maxlen)
